cd47b5ae8024e12d8a9519df91fdfd27816527fd
Rui <mengrui930@gmail.com>
2020-06-11 22:18:58 -0700

    no message

---
+++ server/README.md
@@ -0,0 +1,12 @@
+Low-level request
+- HTTP Module: handle HTTP requests
+
+Routing, Server logic
+- BodyParser: help parse incoming HTTP requests
+- Morgan: logging
+- Express: parse response + routing
+
+Database
+- MongoDB
+- Mongoose
+- Bcrypt: encrypt password
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// DB Setup
+mongoose.connect(
+    'mongodb://localhost:27017/auth',
+    { useNewUrlParser: true,  useUnifiedTopology: true }
+);
+
+
+// App Setup
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json());
+router(app);
+
+
+
+// server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
